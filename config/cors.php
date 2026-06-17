f71212d0203f9b1d0578fddef72cd1114c6e6214
ahmedbeshirdev <info@ahmedbeshir.net>
2026-06-17 12:22:43 +0300

    chore(cors): allow http://localhost:5000 for frontend dev

--- config/cors.php
+++ config/cors.php
@@ -3,7 +3,11 @@
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['http://localhost:3000'],
+    'allowed_origins' => [
+        'http://localhost:3000',
+        'http://localhost:5000',
+        'http://127.0.0.1:5000',
+    ],
     'allowed_origins_patterns' => ['/^https:\/\/.*\.twindix\.com$/'],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
